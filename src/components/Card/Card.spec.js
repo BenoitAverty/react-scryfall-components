946763c723f97d92808f9808c86730c2dd13986a
Benoit Averty <benoit.averty@gmail.com>
2018-09-26 16:29:32 +0200

    doc: add styleguidist

---
+++ src/components/Card/Card.spec.js
@@ -0,0 +1,46 @@
+// Import all that is needed for testing
+import React from 'react'
+import { render, cleanup, waitForElement } from 'react-testing-library'
+import 'jest-dom/extend-expect'
+
+// Import the component under test
+import Card from './'
+
+describe('Card component', () => {
+  beforeEach(() => {
+    cleanup()
+  })
+
+  describe('Rendering a card image', () => {
+    test('snapshot', async () => {
+      const { container } = render(<Card id='eb28b35c-28a5-4042-b21d-6d43658a16eb' />)
+
+      // Wait for an img to appear
+      await waitForElement(() => document.querySelector('img'), { container })
+
+      expect(container.firstChild).toMatchSnapshot()
+    })
+
+    it('renders an image if given a scryfall id', async () => {
+      // Crucible of worlds
+      const { container } = render(<Card id='eb28b35c-28a5-4042-b21d-6d43658a16eb' />)
+
+      // Wait for an img to appear
+      await waitForElement(() => container.querySelector('img'), { container })
+
+      expect(container.querySelectorAll('img')).toHaveLength(1)
+      expect(container.querySelector('img')).toHaveAttribute('src', 'https://img.scryfall.com/cards/normal/en/m19/229.jpg?1531451273')
+    })
+
+    it('renders the small image with the size property at small', async () => {
+      // Momentary blink
+      const { container } = render(<Card id='4206d5f8-edeb-4e0b-8e98-736f6ccdcf99' size={Card.SIZE_SMALL} />)
+
+      // Wait for an img to appear
+      await waitForElement(() => container.querySelector('img'), { container })
+
+      expect(container.querySelectorAll('img')).toHaveLength(1)
+      expect(container.querySelector('img')).toHaveAttribute('src', 'https://img.scryfall.com/cards/small/en/mm3/16.jpg?1517813031')
+    })
+  })
+})
--- src/components/Card/index.js
+++ src/components/Card/index.js
@@ -1,3 +1,45 @@
 import React from 'react'
+import PropTypes from 'prop-types'
+import axios from 'axios'
 
-export default () => <p>This is a card!</p>
+class Card extends React.Component {
+  static SIZE_SMALL = 'small'
+  static SIZE_NORMAL = 'normal'
+  static SIZE_LARGE = 'large'
+
+  static propTypes = {
+    /** 
+     * Scryfall ID of the card to render. This is a uuid that should look like
+     * this : eb28b35c-28a5-4042-b21d-6d43658a16eb 
+     */
+    id: PropTypes.string,
+    /** 
+     * Size of the image to render
+     */
+    size: PropTypes.oneOf([Card.SIZE_SMALL, Card.SIZE_NORMAL, Card.SIZE_LARGE])
+  }
+  static defaultProps = {
+    size: Card.SIZE_NORMAL
+  }
+
+  constructor(props) {
+    super(props)
+
+    this.state = {
+      imgUrl: null
+    }
+  }
+
+  componentDidMount() {
+    axios.get(`https://api.scryfall.com/cards/${this.props.id}`)
+      .then(resp => {
+        this.setState({ imgUrl: resp.data.image_uris[this.props.size] })
+      })
+  }
+
+  render() {
+    return this.state.imgUrl ? <img src={this.state.imgUrl} /> : null
+  }
+}
+
+export default Card
