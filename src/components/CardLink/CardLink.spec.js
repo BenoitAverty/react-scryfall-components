c7962c7dcee502dbf1237955627186a79cf74bd0
Benoit Averty <benoit.averty@gmail.com>
2018-09-28 11:07:30 +0200

    feat(CardLink): show the card in a tooltip on mouse hover

--- src/components/CardLink/CardLink.spec.js
+++ src/components/CardLink/CardLink.spec.js
@@ -1,6 +1,8 @@
 // Import all that is needed for testing
 import React from 'react';
-import { render, cleanup, wait } from 'react-testing-library';
+import {
+  render, cleanup, wait, fireEvent, waitForElement,
+} from 'react-testing-library';
 import 'jest-dom/extend-expect';
 
 import { loadAndApplyFixtures, saveFixtures } from '../../../tests/axiosMocking';
@@ -30,7 +32,6 @@ describe('CardLink component', () => {
     });
 
     it('renders a link to the scryfall search', () => {
-      // Crucible of worlds
       const { container } = render(<CardLink>Black Lotus</CardLink>);
 
       expect(container.querySelectorAll('a')).toHaveLength(1);
@@ -38,11 +39,45 @@ describe('CardLink component', () => {
     });
 
     it('renders a link to the scryfall page after the search is resolved', async () => {
-      // Crucible of worlds
       const { container } = render(<CardLink>Black Lotus</CardLink>);
 
       await wait(() => {
         expect(container.querySelector('a')).toHaveAttribute('href', 'https://scryfall.com/card/vma/4/black-lotus?utm_source=api');
+      });
+    });
+  });
+
+  describe('Card Tooltip', () => {
+    test('snapshot', async () => {
+      const { container, getByText, getByAltText } = render(
+        <CardLink>crucible of worlds</CardLink>,
+      );
+
+      fireEvent.mouseOver(getByText('crucible of worlds'));
+      await waitForElement(() => getByAltText('Crucible of Worlds'));
+      expect(container.firstChild).toMatchSnapshot();
+    });
+    it('shows a card on mouse over', async () => {
+      const { container, getByText } = render(<CardLink>crucible of worlds</CardLink>);
+
+      fireEvent.mouseOver(getByText('crucible of worlds'));
+      await wait(() => {
+        expect(container.querySelector('img')).toHaveAttribute('alt', 'Crucible of Worlds');
+      });
+    });
+
+    it('hides the card on mouse out', async () => {
+      const { container, getByText, getByAltText } = render(
+        <CardLink>crucible of worlds</CardLink>,
+      );
+
+      // mouse enter, Wait for the image to appear then mouse leave
+      fireEvent.mouseOver(getByText('crucible of worlds'));
+      await waitForElement(() => getByAltText('Crucible of Worlds'));
+      fireEvent.mouseOut(getByText('crucible of worlds'));
+
+      await wait(() => {
+        expect(container.querySelector('img')).toBeNull();
       });
     });
   });
--- src/components/CardLink/index.js
+++ src/components/CardLink/index.js
@@ -1,6 +1,38 @@
 import React from 'react';
 import PropTypes from 'prop-types';
+import styled from 'react-emotion';
 import axios from '../../axios';
+
+import Card from '../Card';
+
+const css = {
+  position: 'relative',
+  display: 'inline-block',
+  '& a': {
+    'text-decoration': 'underline dotted',
+  },
+  '.tooltip': {
+    'text-align': 'center',
+    width: '146px',
+    height: '204px',
+    position: 'absolute',
+    'background-color': '#ddd',
+    bottom: '160%',
+    left: '50%',
+    'margin-left': '-73px',
+  },
+  '.tooltip::after': {
+    content: '" "',
+    position: 'absolute',
+    top: '100%', /* At the bottom of the tooltip */
+    left: '50%',
+    'margin-left': '-5px',
+    'border-width': '5px',
+    'border-style': 'solid',
+    'border-color': 'black transparent transparent transparent',
+  },
+};
+const CardLinkContainer = styled('div')(css);
 
 class CardLink extends React.Component {
   static propTypes = {
@@ -14,8 +46,12 @@ class CardLink extends React.Component {
   constructor(props) {
     super(props);
 
+    this.showTooltip = this.showTooltip.bind(this);
+    this.hideTooltip = this.hideTooltip.bind(this);
+
     this.state = {
       scryfallUri: null,
+      showTooltip: false,
     };
   }
 
@@ -24,18 +60,39 @@ class CardLink extends React.Component {
 
     axios.get(`https://api.scryfall.com/cards/named?exact=${cardName}`)
       .then((resp) => {
-        this.setState({ scryfallUri: resp.data.scryfall_uri });
+        this.setState({ scryfallUri: resp.data.scryfall_uri, scryfallId: resp.data.id });
       });
   }
 
+  showTooltip() {
+    this.setState({ showTooltip: true });
+  }
+
+  hideTooltip() {
+    this.setState({ showTooltip: false });
+  }
+
   render() {
-    const { scryfallUri } = this.state;
+    const { scryfallUri, scryfallId, showTooltip } = this.state;
     const { children } = this.props;
 
     const href = scryfallUri || `https://scryfall.com/search?q=${children}`;
 
-    return <a href={href}>{children}</a>;
+    const tooltip = showTooltip && scryfallId ? <span className="tooltip"><Card id={scryfallId} size={Card.SIZE_SMALL} /></span> : null;
+
+    return (
+      <CardLinkContainer
+        onMouseOver={this.showTooltip}
+        onFocus={this.showTooltip}
+        onMouseLeave={this.hideTooltip}
+        onBlur={this.hideTooltip}
+      >
+        {tooltip}
+        <a href={href}>{children}</a>
+      </CardLinkContainer>
+    );
   }
 }
+
 
 export default CardLink;
