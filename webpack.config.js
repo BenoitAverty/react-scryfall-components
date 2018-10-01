0acebbd23c47e881ffdcec32d38cd6c2a4c5c469
Benoit Averty <benoit.averty@gmail.com>
2018-10-01 09:41:30 +0200

    wip

--- webpack.config.js
+++ webpack.config.js
@@ -1,26 +1,30 @@
-const path = require('path')
-const nodeExternals = require('webpack-node-externals')
+const path = require('path');
+const nodeExternals = require('webpack-node-externals');
 
-module.exports = {
-  entry: path.resolve(__dirname, 'src/index.js'),
-  output: {
-    path: path.resolve(__dirname, './dist'),
-    filename: 'react-scryfall-components.umd.js',
-    library: 'ReactScryfallComponents',
-    libraryTarget: 'umd'
-  },
-  externals: [nodeExternals()],
-  module: {
-    rules: [
-      {
-        test: /\.js$/,
-        exclude: /(node_modules|bower_components)/,
-        loader: 'babel-loader',
-      },
-      {
-        test: /\.css$/,
-        use: ['style-loader', 'css-loader']
-      }
-    ]
-  }
+module.exports = (env, argv) => {
+  const mode = argv.mode || 'production';
+  return {
+    entry: path.resolve(__dirname, 'src/index.js'),
+    output: {
+      path: path.resolve(__dirname, './dist'),
+      filename: 'react-scryfall-components.umd.js',
+      library: 'ReactScryfallComponents',
+      libraryTarget: 'umd',
+    },
+    externals: [nodeExternals()],
+    module: {
+      rules: [
+        {
+          test: /\.js$/,
+          exclude: /(node_modules|bower_components)/,
+          loader: 'babel-loader',
+        },
+        {
+          test: /\.css$/,
+          use: ['style-loader', 'css-loader'],
+        },
+      ],
+    },
+    devtool: mode === 'production' ? 'source-map' : 'eval-source-map',
+  };
 };
